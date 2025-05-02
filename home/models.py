2ed7ede10abe1b78081b682d7ba173fd8cb2eeb7
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-05-02 21:07:07 +0100

    Added comments to Home model

--- home/models.py
+++ home/models.py
@@ -2,24 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-STATUS = ((0, "Draft"), (1, "Published"))
+STATUS = ((0, "Draft"), (1, "Reserved"))
 
+# This is the Cottage model
 class Cottage(models.Model):
-    id = models.CharField(primary_key=True, max_length=10)
+    id = models.CharField(primary_key=True, max_length=1)
     sq_ft = models.IntegerField()
     clean_date = models.ForeignKey('Reservation', on_delete=models.SET_NULL, null=True, blank=True, related_name='clean_cottages')
-    water_filter = models.IntegerField(default=False)
-    water_filter_replace = models.DateField()
-    fireplace = models.IntegerField(default=False)
+    water_filter = models.IntegerField(blank=True)
+    water_filter_replace = models.DateField(blank=True)
+    fireplace = models.IntegerField(blank=True)
 
+    # This selects the cottage to go with the date for cleaning
     def __str__(self):
         return f"Cottage {self.id}"
 
-
+# This is the reservation model
 class Reservation(models.Model):
     cottage = models.ForeignKey(Cottage, on_delete=models.CASCADE)
     start = models.DateField()
     end = models.DateField()
 
+    # This displays the cottage id and the date range of the reservation
     def __str__(self):
         return f"{self.cottage.id}: {self.start} → {self.end}"
