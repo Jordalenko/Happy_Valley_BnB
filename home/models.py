e15637ef26c267a18fa2c5c9bb2c9a1e9707d467
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-05-03 19:20:26 +0100

    Move guest model up so it follows cottage model.

--- home/models.py
+++ home/models.py
@@ -27,49 +27,7 @@ class Cottage(models.Model):
         return f"Cottage {self.cottage_id}"
     
 
-# This is the reservation model
-class Reservation(models.Model):
-    cottage = models.ForeignKey(
-        Cottage,
-        on_delete=models.CASCADE,
-        related_name="reserved"
-    )
-    start = models.DateField()
-    end = models.DateField()
-    guest_id = models.ForeignKey(
-        Guest, on_delete=models.CASCADE, related_name="reserver", null=True, 
-        blank=True
-    )
-    discount = models.ForeignKey(
-        Guest,
-        on_delete=models.CASCADE,
-        related_name="reservation_discounts",
-        default=5
-        )
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.cottage.id}: {self.start} → {self.end}"
-    
-
-# This is the completed bookings model
-class Complete(models.Model):
-    cottage = models.ForeignKey(
-        Cottage, on_delete=models.CASCADE,
-        related_name="cottage_completed"
-        )
-    start = models.DateField()
-    end = models.DateField()
-    guest_id = models.ForeignKey(
-        Guest, on_delete=models.CASCADE,
-        related_name="user_completed", null=True, blank=True
-        )
-    discount = models.ForeignKey(
-        Guest, on_delete=models.CASCADE,
-        related_name="complete_discount", default=5
-        )
-
-
+# This is the guest model 
 class Guest(models.Model):
     guest_id = models.AutoField(primary_key=True)
     name = models.TextField()
@@ -80,6 +38,39 @@ class Guest(models.Model):
     discount = models.IntegerField(default=0)
 
 
-# This is the string representation of the completed bookings
-class Meta:
-    ordering = ["-created_on"]
+# This is the reservation model
+class Reservation(models.Model):
+    cottage = models.ForeignKey(
+        Cottage,
+        on_delete=models.CASCADE,
+        related_name="reserved"
+    )
+    start = models.DateField()
+    end = models.DateField()
+    guest_id = models.ForeignKey(
+        'Guest', on_delete=models.CASCADE, related_name="reserver", null=True, 
+        blank=True
+    )
+    discount = models.IntegerField(default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cottage {self.cottage.cottage_id}: {self.start} → {self.end}"
+
+    class Meta:
+        ordering = ["-created_on"]
+    
+
+# This is the completed bookings model
+class Complete(models.Model):
+    cottage = models.ForeignKey(
+        Cottage, on_delete=models.CASCADE,
+        related_name="cottage_completed"
+    )
+    start = models.DateField()
+    end = models.DateField()
+    guest_id = models.ForeignKey(
+        'Guest', on_delete=models.CASCADE,
+        related_name="user_completed", null=True, blank=True
+    )
+    discount = models.IntegerField(default=0)
